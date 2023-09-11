48e6d55e6415ac267cf0a369290737004791d8d4
Daniel4000-dev <107401143+Daniel4000-dev@users.noreply.github.com>
2023-09-11 16:43:35 +0100

    next commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className={``}>
+        <div className={``}>
+            <div className={``}>
+                <div className={``}></div>
+            </div>
+
+            <div className={``}>
+                
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
